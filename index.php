4e36e90ce2142af15c2f6da39824dcb6b82c7751
nadamson <nadamson@seopult.ru>
2020-02-20 23:34:31 +0300

    del file

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-require_once 'vendor/autoload.php';
-use Currency\CurrencyEnum;
-use Currency\Handlers\CurrencyRedis;
-use MyCLabs\Enum\Enum;
-
-$current_currency = new CurrencyEnum(CurrencyEnum::USDRUB);
-
-echo (new CurrencyRedis($current_currency))->getCurrency();
